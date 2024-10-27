a1bcfd65fcba40e80e9f81b70ad902fbce7b92c9
angiemc98 <molinanatalia@hotmail.com>
2024-10-27 12:54:13 -0500

    Movimiento del jugador y de la pelota

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float InitSpeed = 10f;
+    private Rigidbody2D ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GetComponent<Rigidbody2D>();
+        LaunchBall();
+    }
+
+    private void LaunchBall()
+    {
+        Vector2 direction = new Vector2(Random.Range(-1f, 1f), 1).normalized;
+        ball.velocity = direction * InitSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        ball.velocity = ball.velocity.normalized * InitSpeed;
+        
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,27 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+   public Rigidbody2D rb;
+   public float speed = 25f;
+   public float limit= 8f;
+   public float inputValue;
+  
+
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); 
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        inputValue = Input.GetAxis("Horizontal");
+        float PosX = transform.position.x + inputValue*speed* Time.deltaTime;
+
+        PosX = Mathf.Clamp(PosX, -limit, limit);
+
+        transform.position = new Vector2(PosX, transform.position.y);
     }
 }
